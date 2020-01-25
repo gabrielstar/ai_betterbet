a448cc28ab616e219e36b7d65c4549388cc23450
Gabriel Starczewski <gabriel.starczewski@gmail.com>
2020-01-25 09:07:01 +0100

    Dodaj pliki projektów.

---
+++ ai-betterbet/Controlers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GiveNTake.Controlers
+{
+    public class HomeController : Controller
+    {
+        private const String MESSAGE_HEALTHY = "healthy";
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("healthcheck")]
+        [Route("home/healthcheck")]
+        public IActionResult Healtcheck(String mesage = MESSAGE_HEALTHY)
+        {
+            return Content(MESSAGE_HEALTHY);
+        }
+
+        [Route("ping")]
+        public IActionResult Ping()
+        {
+            return Healtcheck("");
+        }
+
+    }
+}
