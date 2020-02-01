c81aeddcf18f96841493d187e476f217b3f8f743
Gabriel Starczewski <gabriel.starczewski@gmail.com>
2020-02-01 23:50:54 +0100

    Reverting namespace change

---
+++ ai-betterbet-tests/Tests/Unit/Messages/MessagesControllerTests.cs
@@ -0,0 +1,62 @@
+using ai_betterbet.Controllers;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace XUnitTestsAiBetterBet
+{
+    /// <summary>
+    /// File reading is an IO operation so we want to do it once for all tests
+    /// Example is rather lousy, we should be reading that from a resource file
+    /// To be refactored, otherwise will produce inconsistent results
+    /// </summary>
+    public class FileReaderFixture : IDisposable
+    {
+        private const string URLS_FILE_NAME = "urls.txt";
+        private const string URLS_FILE_PATH = @"C:\Users\swelse";
+
+        public FileReaderFixture()
+        {
+            try
+            {
+                Urls = System.IO.File.ReadAllLines($@"{URLS_FILE_PATH}\{URLS_FILE_NAME}"); //no escape chars
+            }
+            catch (Exception)
+            {
+                Console.WriteLine($"File {URLS_FILE_NAME} not found. Using defaults.");
+                Urls = new string[] { "https://github.com" };
+            }
+        }
+
+        public void Dispose()
+        {
+            //do nothing, file is closed
+        }
+
+        public string[] Urls { get; private set; }
+    }
+
+    //Our tests will contain Fixture, an object shared in all test in class
+    public class MessagesControllerTests : IClassFixture<FileReaderFixture>
+    {
+        private readonly FileReaderFixture fileReaderFixture;
+        private readonly MessagesController messagesController;
+
+        public MessagesControllerTests(FileReaderFixture fileReaderFixture)
+        {
+            this.fileReaderFixture = fileReaderFixture;
+            this.messagesController = new MessagesController();
+        }
+
+        [Fact]
+        public void MessageContainsGithubURL()
+        {
+            //arrange
+            string githubURL = fileReaderFixture.Urls.FirstOrDefault();
+            //act
+            string message = messagesController.GetMessages();
+            //assert
+            Assert.Contains(githubURL, message);
+        }
+    }
+}
---
+++ ai-betterbet-tests/Tests/Unit/Teams/TeamsServiceTests.cs
@@ -0,0 +1,55 @@
+﻿using ai_betterbet.Model;
+using ai_betterbet.Services;
+using Moq;
+using System.Collections.Generic;
+using Xunit;
+using static ai_betterbet.Services.FinancialService;
+
+namespace XUnitTestsAiBetterBet
+{
+    public class TeamsServiceTests
+    {
+        public class TeamsServiceTestsEmptyListRichTeams
+        {
+            private readonly TeamsService teamsService;
+            private readonly List<Team> teams = new List<Team>();
+            private readonly Mock<IFinancialService> financialServiceMock = new Mock<IFinancialService>();
+
+            public TeamsServiceTestsEmptyListRichTeams()
+            {
+                teams.Add(new Team(1, "Valencia", "1", 1));
+                financialServiceMock.Setup(f => f.GetRichTeams(teams)).Returns(new List<Team>());
+                teamsService = new TeamsService(financialServiceMock.Object);
+            }
+
+            [Fact]
+            public void Get_Rich_Teams_Budget_Should_Return_0_For_Poor_Teams()
+            {
+                Assert.Equal(0, teamsService.GetRichTeamsBudget(teams));
+            }
+        }
+
+        public class TeamsServiceTestsRichTeams
+        {
+            private readonly TeamsService teamsService;
+            private readonly List<Team> teams = new List<Team>();
+            private readonly Mock<IFinancialService> financialServiceMock = new Mock<IFinancialService>();
+
+            public TeamsServiceTestsRichTeams()
+            {
+                teams.Add(new Team(1, "Valencia", "Primera Division", (decimal)FINANCIAL_LEVELS.RICH));
+                teams.Add(new Team(2, "MU", "Premier League", (decimal)FINANCIAL_LEVELS.RICH));
+                teams.Add(new Team(3, "Borussia D", "Bundesliga", (decimal)FINANCIAL_LEVELS.RICH));
+                financialServiceMock.Setup(f => f.GetRichTeams(teams)).Returns(teams);
+                teamsService = new TeamsService(financialServiceMock.Object);
+            }
+
+            [Fact]
+            public void Get_Rich_Teams_Budget_Should_Return_Correct_Result()
+            {
+                Assert.Equal(3*(decimal)(FINANCIAL_LEVELS.RICH), teamsService.GetRichTeamsBudget(teams));
+                financialServiceMock.Verify(mock=>mock.GetRichTeams(teams),Times.Once);
+            }
+        }
+    }
+}
